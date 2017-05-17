5b2a70ef0ca813575aa299dec8bef411306bf9b5
Sabrina Simeroth <ses0607@gmail.com>
2017-05-17 12:35:36 -0700

    removed array

--- models/Photo.js
+++ models/Photo.js
@@ -6,14 +6,14 @@ const PostSchema = new Schema({
     createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
     postDate: { type: Date},
     
-    rating: [{ type: Number, required: true }],
-    question1: [{ type: Boolean, required: false}],
-    question2: [{ type: Boolean, required: false}],
-    question3: [{ type: Boolean, required: false}],
-    question4: [{ type: Boolean, required: false}],
-    question5: [{ type: Boolean, required: false}],
-    question6: [{ type: Boolean, required: false}],
-    notes: [{ type: String, required: false }]
+    rating: { type: Number, required: true },
+    question1: { type: Boolean, required: false},
+    question2: { type: Boolean, required: false},
+    question3: { type: Boolean, required: false},
+    question4: { type: Boolean, required: false},
+    question5: { type: Boolean, required: false},
+    question6: { type: Boolean, required: false},
+    notes: { type: String, required: false }
 });
 
 module.exports = mongoose.model('Post', PostSchema);
